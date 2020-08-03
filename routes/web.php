d25157b605717e74b2e4d781b42d7344b176d07e
margleb <margleb@yandex.ru>
2020-08-03 12:23:34 +0300

    Row SQL Queries из курса по Laravel

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Добавление записи в БД
+Route::get('/insert', function() {
+    DB::insert('insert into posts(title, content) values (?, ?)', ['PHP c Laravel', 'Laravel это лучшее, что произошло с PHP']);
+});
+
+// Прочитывание записи из БД
+Route::get('/read', function() {
+   $results = DB::select('select * from posts where id = ?', [1]);
+   foreach($results as $result) {
+       return $result->title;
+   }
+});
+
+// Обновление данных
+Route::get('/update', function() {
+   $updated = DB::update('update posts set title = "Обновленный тайтл" where id = ?', [1]);
+   return $updated; // возращает id обновленной записи
+});
+
+// Удаление данных
+Route::get('/delete', function() {
+   $deleted = DB::select('delete from posts where id = ?', [1]);
+   return $deleted;
+});
