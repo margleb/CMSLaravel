be96806629ceb2c84afdc016aa555d425039732f
margleb <margleb@yandex.ru>
2020-08-10 18:28:56 +0300

    One to One Relationship СRUD из курса по Laravel

---
+++ database/migrations/2020_08_10_150627_create_addresses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            // UNSIGNED - означает что не может хранить отрицательные числа
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\User;
+use App\Address;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +16,33 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Добавление данных ( связь, один к одному )
+Route::get('/insert', function() {
+    $user = User::find(3);
+    $address = new Address(['name' => 'Санкт-Петерубрг, ул. Шелгунова, д. 12 кв.64']);
+    $user->address()->save($address);
+});
+
+// Обновление данных ( связь, один к одному )
+Route::get('/update', function() {
+    // $address = Address::where('user_id', 1)->first(); // 1ый вариант
+    // $address = Address::where('user_id', '=', 1)->first(); // 2ой вариант
+    $address = Address::whereUserId(3)->first(); // 3ий вариант
+    $address->name = 'Москва, ул. Арбат д. 12, кв. 13';
+    $address->save();
+});
+
+// Прочтение данных ( связь, один к одному )
+Route::get('/read', function() {
+   $user = User::find(3);
+   return $user->address->name;
+});
+
+// Удаление данных ( связь, один к одному )
+Route::get('/delete', function() {
+   $user = User::findOrFail(3);
+   $user->address()->delete();
+   return 'Запись успешно удалена';
 });
